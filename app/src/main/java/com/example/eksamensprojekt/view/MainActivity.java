ec14386626864f476e238c8435b5e65533e69484
MarcKlesiewicz <marcklesiewicz21@gmail.com>
2020-05-13 17:13:10 +0200

    Connected to firebase and set color values

--- app/src/main/java/com/example/eksamensprojekt/view/MainActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.eksamensprojekt;
+package com.example.eksamensprojekt.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.eksamensprojekt.R;
 
 public class MainActivity extends AppCompatActivity {
 
