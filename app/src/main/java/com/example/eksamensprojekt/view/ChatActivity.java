a7b7621b6750e9a1b95503ac55f3fc219a34b598
yokarak <andbille13@gmail.com>
2020-05-19 13:49:08 +0200

    Update ChatActivity.java

--- app/src/main/java/com/example/eksamensprojekt/view/ChatActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/view/ChatActivity.java
@@ -1,43 +1,122 @@
 package com.example.eksamensprojekt.view;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.eksamensprojekt.Fragments.BrugerFragment;
+import com.example.eksamensprojekt.Fragments.ChatsFragment;
 import com.example.eksamensprojekt.R;
+import com.example.eksamensprojekt.model.Bruger;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+
+    TextView brugerNavn;
+    ShapeableImageView profile_billede;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        mAuth = FirebaseAuth.getInstance();
 
+    brugerNavn = findViewById(R.id.brugerNavnChat);
+        profile_billede = findViewById(R.id.profile_billede);
+
+    MaterialToolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("");
+
+
+
+    firebaseUser =FirebaseAuth.getInstance().getCurrentUser();
+    reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Bruger bruger = dataSnapshot.getValue(Bruger.class);
+            brugerNavn.setText(bruger.getBrugerNavn());
+            profile_billede.setImageResource(R.mipmap.ic_launcher);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+    TabLayout tabLayout = findViewById(R.id.tab_layout);
+    ViewPager viewPager = findViewById(R.id.view_paper);
+
+    ViewPagerAdapter viewPagerAdapter =  new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new ChatsFragment(), "Chats");
+        viewPagerAdapter.addFragment(new BrugerFragment(), "Users");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+}
+
+
+
+
+
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragments;
+    private ArrayList<String> titles;
+
+    ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        this.fragments = new ArrayList<>();
+        this.titles = new ArrayList<>();
+    }
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-
-
-        if (currentUser == null) {
-
-            Intent signInIntent = new Intent(ChatActivity.this, LoginInActivity.class);
-            startActivity(signInIntent);
-            finish();
-        }
-
-
+    public int getCount() {
+        return fragments.size();
     }
+
+    public void addFragment( Fragment fragment, String title) {
+        fragments.add(fragment);
+        titles.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+}
 
 }
