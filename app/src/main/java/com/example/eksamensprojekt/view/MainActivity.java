58acb1746299fd546ddc2d0fa955be3300a1a2ca
yokarak <andbille13@gmail.com>
2020-05-21 11:02:10 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/eksamensprojekt/view/MainActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/view/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import com.example.eksamensprojekt.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
@@ -18,23 +20,37 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    FirebaseUser firebaseUser;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        // check if user is null
+        if (firebaseUser != null ) {
+            Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
+       // mAuth = FirebaseAuth.getInstance();
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+        /* Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout); */
 
 
         Button chat = findViewById(R.id.chatBtn);
         chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), ChatActivity.class);
+                Intent i = new Intent(getApplicationContext(), BeskedActivity.class);
                 startActivity(i);
             }
         });
@@ -47,24 +63,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        Button logout = findViewById(R.id.logoutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, OpretBrugerActivity.class));
+                finish();
+            }
+        });
 }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
 
-        /*
-        if not signed in go to sign in activity
-        if (currentUser == null){
-
-            Intent signInIntent = new Intent(MainActivity.this, SignInActivity.class);
-            startActivity(signInIntent);
-            finish();
-        }
-
-         */
-    }
 }
