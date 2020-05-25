d3ef023df456431507d4ba62dacb40b604a378fb
MarcKlesiewicz <marcklesiewicz21@gmail.com>
2020-05-25 11:22:26 +0200

    instanceret forskellige views i visActivity

--- app/src/main/java/com/example/eksamensprojekt/view/VisProfilActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/view/VisProfilActivity.java
@@ -4,18 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.eksamensprojekt.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 import java.util.Objects;
 
 
 public class VisProfilActivity extends AppCompatActivity {
 
-    private Button  mLogudBtn;
+    private Button seTraeningBtn, seBeskederBtn, logUdBtn, bookTidBtn;
+
+    private TextView visFuldenavn, visEmail, visTelefonNr, visNaesteBooking;
+
+    private ImageView actionBarChat, actionBarMain;
 
     private FirebaseAuth mAuth;
 
@@ -30,9 +39,22 @@ public class VisProfilActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mLogudBtn = (Button) findViewById(R.id.log_ud_btn);
+        seTraeningBtn = (Button) findViewById(R.id.goto_traening_btn);
+        seBeskederBtn =(Button) findViewById(R.id.goto_chat_btn);
+        logUdBtn = (Button) findViewById(R.id.logud_btn);
+        bookTidBtn = (Button) findViewById(R.id.goto_booking_btn);
 
-        mLogudBtn.setOnClickListener(new View.OnClickListener() {
+        visFuldenavn = (TextView) findViewById(R.id.vis_fuldenavn_tv);
+        visEmail = (TextView) findViewById(R.id.vis_email_tv);
+        visTelefonNr = (TextView) findViewById(R.id.vis_telefonNr_tv);
+        visNaesteBooking = (TextView) findViewById(R.id.vis_dato_tv);
+
+        actionBarChat = (ImageView) findViewById(R.id.action_bar_chat);
+        actionBarMain = (ImageView) findViewById(R.id.action_bar_logo);
+
+
+        //Sætter de forskellige views til at ændre layoutet
+        logUdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -42,9 +64,56 @@ public class VisProfilActivity extends AppCompatActivity {
 
             }
         });
+
+        seTraeningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, FeedbackActivity.class));
+                finish();
+            }
+        });
+
+        seBeskederBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, ChatActivity.class));
+                finish();
+
+            }
+        });
+
+        bookTidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, BookTidActivity.class));
+                finish();
+            }
+        });
+
+        actionBarChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, ChatActivity.class));
+                finish();
+            }
+        });
+
+        actionBarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
     }
 
-    //Tjekker om bruger er logget ind. Hvis ikke, vises opret bruger activity
+    //Tjekker om bruger er logget ind. Hvis ikke, skifter til opret bruger activity
     @Override
     public void onStart() {
         super.onStart();
