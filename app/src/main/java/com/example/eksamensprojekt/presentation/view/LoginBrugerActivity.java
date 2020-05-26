f49bbc85cc8fbf06af55e1ba9de79f13e7543925
MarcKlesiewicz <marcklesiewicz21@gmail.com>
2020-05-26 12:01:36 +0200

    refactor navne på variabler i login bruger

--- app/src/main/java/com/example/eksamensprojekt/presentation/view/LoginBrugerActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/presentation/view/LoginBrugerActivity.java
@@ -2,38 +2,40 @@ package com.example.eksamensprojekt.presentation.view;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.eksamensprojekt.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 import java.util.Objects;
 
 public class LoginBrugerActivity extends AppCompatActivity {
 
-    private TextInputLayout iEmail;
-    private TextInputLayout iAdgangskode;
+    private TextInputLayout email, adgangskode;
 
-    ImageView actionBarProfil, actionBarChat, actionBarMenu; //Action Bar Variabler
-    private Button mGoToOpretBrugerBtn;
-    private Button mLoginbtn;
-    private Button mStaticBtn;
+    private Button goToOpretBrugerBtn, loginBtn;
 
-    private ProgressDialog mLoginProgress;
+    private ImageView actionBarMain,actionBarChat,actionBarProfil;
+
+    private ProgressDialog loginProgress;
 
     //firebase authentication
     private FirebaseAuth firebaseAuth;
+
+
 
 
     @Override
@@ -43,26 +45,33 @@ public class LoginBrugerActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        //Action Bar
-        //Tilføjer custom action bar til activity
+        //Tilføjer custom actionbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
 
-        //Forbinder IDs til de korrekte views
-        actionBarProfil = (ImageView) findViewById(R.id.action_bar_profil);
+        actionBarMain = (ImageView) findViewById(R.id.action_bar_logo);
         actionBarChat = (ImageView) findViewById(R.id.action_bar_chat);
-        actionBarMenu = (ImageView) findViewById(R.id.action_bar_logo);
+        actionBarProfil = (ImageView) findViewById(R.id.action_bar_profil);
 
-        //Skifter til vis profil activity
+
+        actionBarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginBrugerActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
         actionBarProfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 startActivity(new Intent(LoginBrugerActivity.this, VisProfilActivity.class));
                 finish();
             }
         });
 
-        //Skifter til chat activity
         actionBarChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,44 +81,34 @@ public class LoginBrugerActivity extends AppCompatActivity {
             }
         });
 
-        //Skifter til menu activity
-        actionBarMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        // ^^action bar
 
-                startActivity(new Intent(LoginBrugerActivity.this, MainActivity.class));
-                finish();
-            }
-        });
-        // ^ Action bar ^
 
         //Skaber new progress dialog
-        mLoginProgress = new ProgressDialog(this);
+        loginProgress = new ProgressDialog(this);
 
-        mGoToOpretBrugerBtn = (Button) findViewById(R.id.goto_opret_bruger_btn);
-        mLoginbtn = (Button) findViewById(R.id.login_paa_bruger);
-        mStaticBtn = (Button) findViewById(R.id.static_goto_login_btn);
+        //Sætter ids til de korrekte views
+        goToOpretBrugerBtn = (Button) findViewById(R.id.goto_opret_bruger_btn);
+        loginBtn = (Button) findViewById(R.id.login_paa_bruger);
 
-
-        //TextInput for login xml
-        iEmail = (TextInputLayout) findViewById(R.id.indsaetEmail);
-        iAdgangskode = (TextInputLayout) findViewById(R.id.indsaetAdgangskode);
+        email = (TextInputLayout) findViewById(R.id.indsaetEmail);
+        adgangskode = (TextInputLayout) findViewById(R.id.indsaetAdgangskode);
 
 
-        //tjekker om felterne er udfyldt og  henter bruger
-        mLoginbtn.setOnClickListener(new View.OnClickListener() {
+        //Tjekker om felterne er udfyldt og  henter bruger
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String indsatEmail = iEmail.getEditText().getText().toString();
-                String indsatAdgangskode = iAdgangskode.getEditText().getText().toString();
+                String indsatEmail = email.getEditText().getText().toString();
+                String indsatAdgangskode = adgangskode.getEditText().getText().toString();
 
                 if (!TextUtils.isEmpty(indsatEmail) || !TextUtils.isEmpty(indsatAdgangskode)) {
 
-                    mLoginProgress.setTitle("Logger ind");
-                    mLoginProgress.setMessage("Vent venligst mens vi finder din bruger.");
-                    mLoginProgress.setCanceledOnTouchOutside(false);
-                    mLoginProgress.show();
+                    loginProgress.setTitle("Logger ind");
+                    loginProgress.setMessage("Vent venligst mens vi finder din bruger.");
+                    loginProgress.setCanceledOnTouchOutside(false);
+                    loginProgress.show();
 
                     hentBruger(indsatEmail, indsatAdgangskode);
 
@@ -119,7 +118,7 @@ public class LoginBrugerActivity extends AppCompatActivity {
         });
 
         //skifter til opret bruger activity
-        mGoToOpretBrugerBtn.setOnClickListener(new View.OnClickListener() {
+        goToOpretBrugerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -137,24 +136,26 @@ public class LoginBrugerActivity extends AppCompatActivity {
         firebaseAuth.signInWithEmailAndPassword(indsatEmail, indsatAdgangskode).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
+
+                if (task.isSuccessful()){
 
 
-                    mLoginProgress.dismiss();
+                    loginProgress.dismiss();
                     Intent mainIntent = new Intent(LoginBrugerActivity.this, MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(mainIntent);
                     finish();
-                } else {
+                }else {
 
-                    mLoginProgress.hide();
+                    loginProgress.hide();
                     Toast.makeText(LoginBrugerActivity.this, "Der opstod en fejl. Email eller adgangskode er forkert", Toast.LENGTH_LONG).show();
                 }
 
             }
         });
 
-
     }
 }
+
+
+
 
