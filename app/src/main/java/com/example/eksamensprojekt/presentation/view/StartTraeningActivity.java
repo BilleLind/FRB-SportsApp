a768aa3288b21ab3d105eee16e0b5b5786e3bd2a
CodeyFlex <Sej920@hotmail.com>
2020-05-29 01:55:26 +0200

    Added proper naming to each exercise

--- app/src/main/java/com/example/eksamensprojekt/presentation/view/StartTraeningActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/presentation/view/StartTraeningActivity.java
@@ -7,6 +7,7 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.eksamensprojekt.R;
@@ -23,7 +24,8 @@ public class StartTraeningActivity extends AppCompatActivity {
     private WebView oovelseWebViewVar;
     private Button naesteOovelseButton, gennemfoortButton;
     private ImageView actionBarProfil, actionBarChat, actionBarMenu; //Action Bar Variabler
-    private String webViewURL = "https://exorlive.com/video/?culture=da-DK&ex=12498";
+    private String webViewURL = "https://exorlive.com/video/?culture=da-DK&ex=11";
+    private String webViewName;
     private int oovelsesPosition = 0;
 
     @Override
@@ -70,26 +72,36 @@ public class StartTraeningActivity extends AppCompatActivity {
         // ^ Action bar ^
 
         final ArrayList oovelsesListe = new ArrayList(); //Primitiv liste over øvelser
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12492");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12493");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12494");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12495");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12496");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12497");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12490");
-        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=12491");
+        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=605");
+        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=711");
+        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=29");
+        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=16");
+        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=8820");
+        oovelsesListe.add("https://exorlive.com/video/?culture=da-DK&ex=10306");
+
+        final ArrayList oovelsesNavnListe = new ArrayList(); //Primitiv liste over navn på øvelser
+        oovelsesNavnListe.add("Etbens knæbøj");
+        oovelsesNavnListe.add("Bækkenløft m/knæstræk");
+        oovelsesNavnListe.add("Armstræk");
+        oovelsesNavnListe.add("Mavebøjning");
+        oovelsesNavnListe.add("Lateral lunge");
+        oovelsesNavnListe.add("Hoppende knæbøjninger");
 
         naesteOovelseButton = (Button)findViewById(R.id.naeste_Oovelse_Button);
+        titleTextView = (TextView) findViewById(R.id.title_Text_View);
 
         //Skifter til ny øvelse
         naesteOovelseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 webViewURL = (String) oovelsesListe.get(oovelsesPosition);
+                webViewName = (String) oovelsesNavnListe.get(oovelsesPosition);
+                titleTextView.setText(webViewName);
                 oovelseWebViewVar.loadUrl(webViewURL);
                 if (oovelsesPosition <= 4) {
                     oovelsesPosition = oovelsesPosition + 1;
                 } else{
+                    Toast.makeText(getApplicationContext(),"Du har gennemført dagens program, godt klaret!",Toast.LENGTH_LONG).show();
                     startActivity(new Intent(StartTraeningActivity.this, FeedbackActivity.class));
                     finish();
                 }
