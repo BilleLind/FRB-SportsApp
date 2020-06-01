4b5c5bec568c5cbaa6fd092593cc9033f19c2730
yokarak <andbille13@gmail.com>
2020-06-01 10:34:58 +0200

    rettede en String navn + lidt kommentare

--- app/src/main/java/com/example/eksamensprojekt/presentation/adapter/BeskedAdapter.java
+++ app/src/main/java/com/example/eksamensprojekt/presentation/adapter/BeskedAdapter.java
@@ -19,8 +19,8 @@ import java.util.List;
 
 public class BeskedAdapter extends RecyclerView.Adapter<BeskedAdapter.ViewHolder> {
 
-    public static final int MSG_TYPE_LEFT = 0;
-    public static final int MSG_TYPE_RIGHT = 1;
+    public static final int MSG_TYPE_VENSTRE = 0;
+    public static final int MSG_TYPE_HOEJRE = 1;
 
     private Context context;
     private List<Besked> beskedList;
@@ -37,7 +37,7 @@ public class BeskedAdapter extends RecyclerView.Adapter<BeskedAdapter.ViewHolder
     @NonNull
     @Override
     public BeskedAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { // vælger om at den skal vises i view 1 (højre) eller 0 (venstre)
-        if (viewType == MSG_TYPE_RIGHT) {
+        if (viewType == MSG_TYPE_HOEJRE) {
             View view = LayoutInflater.from(context).inflate(R.layout.chat_item_hoejre, parent, false);
             return new BeskedAdapter.ViewHolder(view);
         } else {
@@ -53,7 +53,7 @@ public class BeskedAdapter extends RecyclerView.Adapter<BeskedAdapter.ViewHolder
 
         holder.visBesked.setText(besked.getBesked());
 
-        /*TODO if (billedeURL.equals("default")){
+        /* if (billedeURL.equals("default")){
             holder.profile_billede.setImageResource(R.mipmap.ic_launcher);
         } else {
             Glide.with(mcontext).load(billedeURL).into(holder.profile_billede);
@@ -84,10 +84,10 @@ public class BeskedAdapter extends RecyclerView.Adapter<BeskedAdapter.ViewHolder
     @Override
     public int getItemViewType(int position) {
         firebaseBruger = FirebaseAuth.getInstance().getCurrentUser();
-        if (beskedList.get(position).getAfsender().equals(firebaseBruger.getUid())) {
-            return MSG_TYPE_RIGHT;
+        if (beskedList.get(position).getAfsender().equals(firebaseBruger.getUid())) { // her skelnes mellem afsender og modtager. 
+            return MSG_TYPE_HOEJRE; // højre for afsender
         } else {
-            return MSG_TYPE_LEFT;
+            return MSG_TYPE_VENSTRE; // venstre for modtager
         }
     }
 }
