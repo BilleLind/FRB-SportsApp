ffef4194134f1751038a2ad565d6003377e6181f
yokarak <andbille13@gmail.com>
2020-06-17 14:10:07 +0200

    tilføjede søg funktion
    det er lidt langsomere, burde blive forbedret når jeg ændrer hvordan dataen bliver indexet

--- app/src/main/java/com/example/eksamensprojekt/presentation/fragments/BrugerFragment.java
+++ app/src/main/java/com/example/eksamensprojekt/presentation/fragments/BrugerFragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.eksamensprojekt.R;
 import com.example.eksamensprojekt.presentation.adapter.BrugerAdapter;
@@ -20,6 +24,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -38,6 +43,7 @@ public class    BrugerFragment extends Fragment {
     private BrugerAdapter brugerAdapter;
     private List<Bruger> brugerList;
 
+    EditText soeg_brugere;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,7 +59,52 @@ public class    BrugerFragment extends Fragment {
         visBrugere();
 
 
+        soeg_brugere = view.findViewById(R.id.soege_bruger);
+        soeg_brugere.setInputType(8192); // 8192 som inputType gør så at det første character bliver stort. det er for at gøre det lettere at søge, med den måde som fornavn bliver gemt på.
+        soeg_brugere.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                soegEfterBrugere(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         return view;
+    }
+
+    private void soegEfterBrugere(String s) {
+        final FirebaseUser firebaseBruger = FirebaseAuth.getInstance().getCurrentUser();
+        Query query = FirebaseDatabase.getInstance().getReference("Brugere").orderByChild("fornavn").startAt(s).endAt(s +"\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                brugerList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Bruger bruger = snapshot.getValue(Bruger.class);
+
+                    if (!bruger.getId().equals(firebaseBruger.getUid())) {
+                        brugerList.add(bruger);
+                    }
+                }
+                brugerAdapter = new BrugerAdapter(getContext(), brugerList);
+                recyclerView.setAdapter(brugerAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
@@ -65,19 +116,21 @@ public class    BrugerFragment extends Fragment {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                brugerList.clear();
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Bruger bruger =  snapshot.getValue(Bruger.class);
+                if (soeg_brugere.getText().toString().trim().equals("")) {
+                    brugerList.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Bruger bruger = snapshot.getValue(Bruger.class);
 
-                    assert firebaseBruger != null;
-                    assert bruger != null;
-                    if (!bruger.getId().equals(firebaseBruger.getUid())) { // asserts if there are any other user than the current logged in and adds them to the user fragment activity
-                        brugerList.add(bruger);
+                        assert firebaseBruger != null;
+                        assert bruger != null;
+                        if (!bruger.getId().equals(firebaseBruger.getUid())) { // asserts if there are any other user than the current logged in and adds them to the user fragment activity
+                            brugerList.add(bruger);
+                        }
+
                     }
-
+                    brugerAdapter = new BrugerAdapter(getContext(), brugerList);
+                    recyclerView.setAdapter(brugerAdapter);
                 }
-                brugerAdapter = new BrugerAdapter(getContext(), brugerList);
-                recyclerView.setAdapter(brugerAdapter);
             }
 
             @Override
