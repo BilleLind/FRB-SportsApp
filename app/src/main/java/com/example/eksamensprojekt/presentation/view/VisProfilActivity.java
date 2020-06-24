2e019b5f4501b4499ba4468818356d09c80ae006
yokarak <andbille13@gmail.com>
2020-06-24 18:11:20 +0200

    hurtig gemme pga. tjek i anden branch

--- app/src/main/java/com/example/eksamensprojekt/presentation/view/VisProfilActivity.java
+++ app/src/main/java/com/example/eksamensprojekt/presentation/view/VisProfilActivity.java
@@ -1,58 +1,106 @@
 package com.example.eksamensprojekt.presentation.view;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.eksamensprojekt.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
 import java.util.Objects;
 
+import static com.example.eksamensprojekt.presentation.Interface.Konstante.brugere;
+
+
 public class VisProfilActivity extends AppCompatActivity {
-    /**
-     * @author Anders, Sebastian og Marc
-     * @version 1.2
-     */
 
-    private Button brugerLogUdKnap;
+    private Button seTraeningBtn, seBeskederBtn, logUdBtn, bookTidBtn;
 
-    private FirebaseAuth firebaseAuth;
-    FirebaseUser firebaseBruger;
-    ImageView actionBarProfil, actionBarChat, actionBarMenu; //Action Bar Variabler
+    private TextView visFuldenavn, visEmail, visTelefonNr, visNaesteBooking;
 
+    private ImageView actionBarChat, actionBarMain;
 
+    private FirebaseAuth mAuth;
+    private FirebaseUser fireBruger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vis_profil);
 
-
-        //Action Bar
-        //Tilføjer custom action bar til activity
+        //Tilføjer custom action bar
         Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
 
-        //Forbinder IDs til de korrekte views
-        actionBarProfil = (ImageView) findViewById(R.id.action_bar_profil);
-        actionBarChat = (ImageView) findViewById(R.id.action_bar_chat);
-        actionBarMenu = (ImageView) findViewById(R.id.action_bar_logo);
+        mAuth = FirebaseAuth.getInstance();
+        fireBruger = FirebaseAuth.getInstance().getCurrentUser();
 
-        //Skifter til vis profil activity
-        actionBarProfil.setOnClickListener(new View.OnClickListener() {
+        seTraeningBtn = (Button) findViewById(R.id.goto_traening_btn);
+        seBeskederBtn =(Button) findViewById(R.id.goto_chat_btn);
+        logUdBtn = (Button) findViewById(R.id.logud_btn);
+        bookTidBtn = (Button) findViewById(R.id.goto_booking_btn);
+
+        visFuldenavn = (TextView) findViewById(R.id.vis_fuldenavn_tv);
+        visEmail = (TextView) findViewById(R.id.vis_email_tv);
+        visTelefonNr = (TextView) findViewById(R.id.vis_telefonNr_tv);
+        visNaesteBooking = (TextView) findViewById(R.id.vis_dato_tv);
+
+        actionBarChat = (ImageView) findViewById(R.id.action_bar_chat);
+        actionBarMain = (ImageView) findViewById(R.id.action_bar_logo);
+
+
+        //Sætter de forskellige views til at ændre layoutet
+        logUdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(VisProfilActivity.this, VisProfilActivity.class));
+
+                mAuth.getInstance().signOut();
+                startActivity(new Intent(VisProfilActivity.this, MainActivity.class));
+                finish();
+
+            }
+        });
+
+        seTraeningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, FeedbackActivity.class));
                 finish();
             }
         });
 
-        //Skifter til chat activity
+        seBeskederBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, ChatActivity.class));
+                finish();
+
+            }
+        });
+
+        bookTidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(VisProfilActivity.this, BookTidActivity.class));
+                finish();
+            }
+        });
+
         actionBarChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,8 +110,7 @@ public class VisProfilActivity extends AppCompatActivity {
             }
         });
 
-        //Skifter til menu activity
-        actionBarMenu.setOnClickListener(new View.OnClickListener() {
+        actionBarMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -71,40 +118,35 @@ public class VisProfilActivity extends AppCompatActivity {
                 finish();
             }
         });
-        // ^ Action bar ^
 
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        brugerLogUdKnap = (Button) findViewById(R.id.log_ud_btn);
-
-
-        brugerLogUdKnap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(VisProfilActivity.this, MainActivity.class));
-                finish();
-            }
-        });
     }
 
-    //Tjekker om bruger er logget ind. Hvis ikke, bliver bruger præsenteret for opret bruger aktiviteten.
+    private void startAcitivityMetode(Context context, Context context2) {
+        startActivity(new Intent(context.class, context2.class));
+    }
+
+
+    private void udfyldProfil() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(brugere).child()
+    }
+
+
+
+    //Tjekker om bruger er logget ind. Hvis ikke, skifter til opret bruger activity
     @Override
     public void onStart() {
         super.onStart();
-
-        // Tjek om bruger er logged in (ikke null) og opdater UI som nødvendigt.
-        firebaseBruger = firebaseAuth.getCurrentUser();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
 
-        if (firebaseBruger == null) {
 
-            Intent ikkeLoggetIndIntent = new Intent(VisProfilActivity.this, OpretBrugerActivity.class);
-            startActivity(ikkeLoggetIndIntent);
+        if (currentUser == null) {
+
+            Intent opretBrugerIntent = new Intent(VisProfilActivity.this, OpretBrugerActivity.class);
+            startActivity(opretBrugerIntent);
             finish();
         }
-    }
 
+    }
 }
